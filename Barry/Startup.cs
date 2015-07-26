526301cee23f5c7c411d9c4d4f1c1e40d94612d3
JFong <lordfong@hotmail.com>
2015-07-26 16:25:12 +0200

    Start of the Barry project

---
+++ Barry/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Barry.Startup))]
+namespace Barry
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
